2e9455f09184d558e47539120b5e79d09821160b
Warren Turner <woz@pi.hole>
2026-01-22 19:46:33 +1100

    Update TankLevel project - 2026-01-22

--- lib/device/device.cpp
+++ lib/device/device.cpp
@@ -1,24 +1,32 @@
 #include "mqtt.h"
 #include <Wire.h>
 #include <WiFi.h>
-#include <Adafruit_INA219.h>
 #include <ArduinoJson.h>
 #include "device.h"
 
+#include <Adafruit_BMP280.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include "../telnet/telnet.h"
-#include "ACS712.h"
+
 #define On_Board_LED_PIN 2
 #define SampleTime 5000
+#define ONE_WIRE_BUS 4
+#define INPIN 35
 
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor
+DallasTemperature sensors(&oneWire);
 
 Device::Device() {}
 
 Device device;
 
 const int sensorIn = 34; // pin where the OUT pin from sensor is connected on Arduino
+#define SampleCount 16
 
-
-Adafruit_INA219 ina219;
 // Define static member variables
 bool Device::payloadReady = false;
 char Device::globalBuf[256] = {0};
@@ -31,12 +39,24 @@ void Device::setup()
     mqtt.set_callback(message_handler);
 
     // Publish initial status
-    mqtt.publish("filterchlorine/status", "online");
-    mqtt.publish("filterchlorine/ota/state", "ready");
-    if (!ina219.begin())
+    mqtt.publish("tanklevel/status", "online");
+    mqtt.publish("tanklevel/ota/state", "ready");
+
+    // if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
+    if (!bmp.begin())
     {
-        telnet.println("Failed to find INA219 chip mark as down");
+        Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                         "try a different address!"));
     }
+    else
+        _BMP_Found = true;
+
+    /* Default settings from datasheet. */
+    bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
+                    Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                    Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                    Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                    Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 }
 
 void Device::loop()
@@ -45,26 +65,49 @@ void Device::loop()
 
     int rssi = WiFi.RSSI(); // Get WiFi signal strength
 
-    update_power(); // Read power data from INA219
+    float LocalTemp = 0.0;
+    float LocalPress = 0.0;
+    if (_BMP_Found && bmp.takeForcedMeasurement())
+    {
+        // can now print out the new measurements
+        Serial.print(F("Temperature = "));
+        LocalTemp = bmp.readTemperature();
+        Serial.print(LocalTemp);
+        Serial.println(" *C");
 
+        Serial.print(F("Pressure = "));
+        LocalPress = bmp.readPressure() / 100 + 76.66;
+        Serial.print(LocalPress); //- 1014.0 937.73.34 );
+        Serial.println(" hPa");
+
+        Serial.print(F("Approx altitude = "));
+        Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+        Serial.println(" m");
+
+        Serial.println();
+    }
+    else
+    {
+        Serial.println("Forced measurement failed!");
+    }
+
+    int AverageTankLevel = GetAverageTankLevel();
     // Build JSON payload using ArduinoJson
-    JsonDocument doc;
-    doc["resistance"] = _resistance;
-    doc["current"] = _current_mA;
-    doc["rssi"] = rssi;
-    doc["uptime"] = millis() / 1000;
-    doc["down"] = IsDown() ? "offline" : "online";
-    doc["busvoltage"] = _busvoltage;
-    doc["shuntvoltage"] = _shuntvoltage;
-    doc["loadvoltage"] = _loadvoltage;
-    doc["power_mW"] = _power_mW;
+       JsonDocument doc;
+       doc["outsidetemperature"] = ReadTemp();
+       doc["insidetemperature"] = LocalTemp;
+       doc["pressure"] = LocalPress;
+       doc["rssi"] = rssi;
+       doc["uptime"] = millis() / 1000;
+       doc["down"] = IsDown() ? "offline" : "online";
+       doc["tanklevel"] = AverageTankLevel;
 
-    char jsonBuffer[256];
-    serializeJson(doc, jsonBuffer);
+       char jsonBuffer[256];
+       serializeJson(doc, jsonBuffer);
 
-    // Publish JSON to single topic
-    mqtt.publish("filterchlorine/sensors", jsonBuffer);
-
+       // Publish JSON to single topic
+       mqtt.publish("tanklevel/sensors", jsonBuffer);
+    Serial.println("Published sensor data to MQTT:");
     delay(2000);
     if (payloadReady)
     {
@@ -85,10 +128,10 @@ void Device::message_handler(char *topic, char *payload)
     if (strcmp(payload, "OTA_UPDATE") == 0 || strcmp(payload, "UPDATE") == 0)
     {
         telnet.println("\tOTA Update triggered via MQTT");
-        mqtt.publish("filterchlorine/ota/state", "updating");
+        mqtt.publish("tanklevel/ota/state", "updating");
         telnet.println("\tWaiting for OTA upload...");
         telnet.println("\tDevice is ready for OTA updates");
-        mqtt.publish("filterchlorine/ota/state", "ready");
+        mqtt.publish("tanklevel/ota/state", "ready");
         return;
     }
 
@@ -99,28 +142,37 @@ bool Device::IsDown()
 {
     return _Down;
 }
-void Device::update_power()
+/////////////////////////////////////////////////////////////////
+
+int Device::GetTankLevel()
 {
-    // Read voltage and current from INA219.
-     _shuntvoltage = ina219.getShuntVoltage_mV();
-     _busvoltage = ina219.getBusVoltage_V();
-     _current_mA = ina219.getCurrent_mA();
+    // read the analog / millivolts value for pin 2:
+    int analogVolts = analogReadMilliVolts(INPIN);
+    long tanklevel = map(analogVolts, 600, 2840, 0, 1550);
 
-    // Compute load voltage, power, and milliamp-hours.
-     _loadvoltage = _busvoltage + (_shuntvoltage / 1000);
-     _power_mW = _loadvoltage * _current_mA;
+    return tanklevel;
+}
+int Device::GetAverageTankLevel()
+{
+    int Average = 0;
+    for (int i = 0; i < SampleCount; i++)
+    {
+        Average += GetTankLevel();
+        delay(100); // delay(0.1 second);
+    }
+    // print out the values you read:
+    telnet.println("ADC millivolts value = " + String(Average));
 
-    _total_mA += _current_mA;
-    _total_sec += 1;
-    _total_mAH = _total_mA / 3600.0;
-
-    telnet.print("I: ");
-    telnet.print(_current_mA);
-    telnet.print(" mA, V: ");
-    telnet.print(_loadvoltage);
-    telnet.print(" V, P: ");
-    telnet.print(_power_mW);
-    telnet.print(" mW, mAH: ");
-    telnet.print(_total_mAH);
-    telnet.println(" mAH");
+    return Average / SampleCount;
+}
+float Device::ReadTemp(void)
+{
+    float rv = 0.0;
+    // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+    sensors.requestTemperatures();
+    telnet.print("Celsius temperature: ");
+    // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+    rv = sensors.getTempCByIndex(0);
+    telnet.println(String(rv));
+    return rv;
 }
--- lib/device/device.h
+++ lib/device/device.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <Adafruit_INA219.h>
+#include <Adafruit_BMP280.h>
 
 class Device {
 
@@ -12,25 +12,17 @@ class Device {
         static void message_handler(char *, char *);
         static bool payloadReady;
         static char globalBuf[256];
-        void CalculateData();
 
-        void update_power();
         bool IsDown();
+
+        int GetTankLevel();
+        int GetAverageTankLevel();
+        float ReadTemp(void);
+
     private:
-        float _amps = 0.0;
-        float _resistance = 0.0;
-        Adafruit_INA219 ina219;
-        bool _Down = false;
-        float _total_mA = 0.0;
-        float _total_sec = 0.0;
-
-        float _shuntvoltage = 0.0;
-        float _busvoltage = 0.0;
-        float _current_mA = 0.0;
-        float _loadvoltage = 0.0;
-        float _power_mW = 0.0;
-        float _total_mAH = 0.0;
-
+        bool _Down;
+        bool _BMP_Found;
+        Adafruit_BMP280 bmp; // I2C
 };
 
 
